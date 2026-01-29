beb4e323ce82e136ef589761fcdba6faba52895f
michal92299 <chudybyczek2137@gmail.com>
2026-01-29 20:10:23 +0100

    Create electron-main.js

---
+++ bark-squadron/electron-main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: path.join(__dirname, 'icon.png')
+  });
+
+  // In a real build, this would serve the built index.html
+  // For this environment, we assume serving locally or loading file
+  win.loadURL('http://localhost:3000'); 
+  // Or: win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
