e720cb9bfef5de5ae3fc5bc95a2fdbfe95650e46
michal92299 <chudybyczek2137@gmail.com>
2026-04-10 11:57:08 +0200

    Delete app/electron directory

--- app/electron/preload.ts
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    launchGame: (gameId: string) => ipcRenderer.invoke('launch-game', gameId),
-                                checkGameExists: (gameId: string) => ipcRenderer.invoke('check-game-exists', gameId),
-});
