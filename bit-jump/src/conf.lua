ea4c944635788dfb97b5c956831d46dcd47ff891
michal92299 <chudybyczek2137@gmail.com>
2026-06-22 11:27:52 +0200

    Update conf.lua

--- bit-jump/src/conf.lua
+++ bit-jump/src/conf.lua
@@ -1,9 +1,9 @@
 function love.conf(t)
-t.window.width = 800
-t.window.height = 600
+t.window.width     = 800
+t.window.height    = 600
 t.window.resizable = true
 t.window.fullscreen = false
-t.window.title = "Bit Jump"
-t.modules.audio = false
-t.modules.sound = false
+t.window.title     = "Bit Jump"
+t.modules.audio    = false
+t.modules.sound    = false
 end
